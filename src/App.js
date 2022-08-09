ed78687b4cb40c74f3f87228b35d164399d9b279
Asilbek2002 <school2924@gmail.com>
2022-08-10 01:28:44 +0500

    table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import "./App.css";
-import Calculate from "./components/table";
+import React, { Component } from "react";
+import Table from "./components/table";
 
-function App() {
-  return <Calculate/>
+export default class App extends Component {
+  handleThead = (number) => {
+    const thead = [];
+    for (let i = 0; i < number; i++) {
+      thead.push(<th>Column {i}</th>);
+    }
+    return thead;
+  };
+
+  handleTable = (count) => {
+    const table = [];
+    for (let i = 0; i < count; i++) {
+      table.push(
+        <tr>
+          <td>Edward</td>
+          <td>32</td>
+          <td>London Park No {i}</td>
+          <td>London Park No {i}</td>
+          <td>London Park No {i}</td>
+          <td>London Park No {i}</td>
+          <td>London Park No {i}</td>
+          <td>London Park No {i}</td>
+          <td>London Park No {i}</td>
+          <td>London Park No {i}</td>
+        </tr>
+      );
+    }
+    return table;
+  };
+  render() {
+    return (
+      <div className="wrapper">
+        <Table onThLength={this.handleThead} onTableCount={this.handleTable} />
+      </div>
+    );
+  }
 }
-
-export default App;
