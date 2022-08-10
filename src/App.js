79235401c5362c6fa0a9515c7b33f83f0a91a19f
Asilbek2002 <school2924@gmail.com>
2022-08-10 22:49:49 +0500

    tab project

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import React, { Component } from "react";
-import Table from "./components/table";
 
-export default class App extends Component {
-  handleThead = (number) => {
-    const thead = [];
-    for (let i = 0; i < number; i++) {
-      thead.push(<th>Column {i}</th>);
-    }
-    return thead;
-  };
-
-  handleTable = (count) => {
-    const table = [];
-    for (let i = 0; i < count; i++) {
-      table.push(
-        <tr>
-          <td>Tom</td>
-          <td>10</td>
-          <td>London Park No {i}</td>
-          <td>London Park No {i}</td>
-          <td>London Park No {i}</td>
-          <td>London Park No {i}</td>
-          <td>London Park No {i}</td>
-          <td>London Park No {i}</td>
-          <td>London Park No {i}</td>
-          <td>London Park No {i}</td>
-        </tr>
-      );
-    }
-    return table;
-  };
+class App extends Component {
   render() {
-    return (
-      <div className="wrapper">
-        <Table onThLength={this.handleThead} onTableCount={this.handleTable} />
-      </div>
-    );
+    return <div>App</div>;
   }
 }
+
+export default App;
