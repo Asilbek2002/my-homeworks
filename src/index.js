afa58a2f36fc6fe43b329be6cd40d2c40d621c28
Asilbek2002 <school2924@gmail.com>
2022-08-11 14:18:14 +0500

    react routing without rrd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app";
 import "./assets/base.scss"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
