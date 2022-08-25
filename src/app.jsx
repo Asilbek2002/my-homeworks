13e19398c8407e40c628d807d03f3de100651861
Asilbek2002 <school2924@gmail.com>
2022-08-25 17:53:59 +0500

    new

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,7 @@ import React, { Component } from "react";
 
 export class App extends Component {
   render() {
-    return (
-      <div className="slider">
-        <div className="slider__track" />
-        <div className="slider__range" />
-      </div>
-    );
+    return <App />;
   }
 }
 
