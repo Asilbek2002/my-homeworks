07c40af847ed7d6404d2e4248b472de127dceb42
Asilbek2002 <school2924@gmail.com>
2022-08-25 17:46:02 +0500

    trash code

--- src/app.jsx
+++ src/app.jsx
@@ -1,56 +1,12 @@
 import React, { Component } from "react";
-import NavBar from "./components/navbar";
-import { AboutUs, Services, Products, TicTacToe, Home } from "./pages";
 
-class App extends Component {
-  state = {
-    routes: [
-      { title: "Products", path: "/products" },
-      { title: "Services", path: "/services" },
-      { title: "Tic-Tac-Toe", path: "/tic-tac-toe" },
-      { title: "About Us", path: "/about-us" },
-    ],
-    pathname: window.location.pathname,
-  };
-
-  getPage = () => {
-    const { pathname, routes } = this.state;
-    const children = JSON.stringify(
-      routes.filter(({ path }) => path !== pathname),
-      null,
-      4
-    );
-
-    switch (pathname) {
-      case "/products":
-        return <Products children={children} />;
-      case "/services":
-        return <Services children={children} />;
-      case "/tic-tac-toe":
-        return <TicTacToe children={children} />;
-      case "/about-us":
-        return <AboutUs children={children} />;
-      default:
-        return <Home children={children} />;
-    }
-  };
-
-  handleRoute = (pathname) => {
-    window.history.pushState("data", pathname.toUpperCase(), pathname);
-    this.setState({ pathname });
-  };
-
+export class App extends Component {
   render() {
-    const { pathname, routes } = this.state;
     return (
-      <>
-        <NavBar
-          pathname={pathname}
-          routes={routes}
-          onRoute={this.handleRoute}
-        />
-        <div className="container py-5">{this.getPage()}</div>
-      </>
+      <div className="slider">
+        <div className="slider__track" />
+        <div className="slider__range" />
+      </div>
     );
   }
 }
---
+++ src/components/slider.jsx
@@ -0,0 +1,20 @@
+const MultiRangeSlider = () => {
+  return (
+    <>
+      <input
+        type="range"
+        min="0"
+        max="1000"
+        className="thumb thumb--zindex-3"
+      />
+      <input
+        type="range"
+        min="0"
+        max="1000"
+        className="thumb thumb--zindex-4"
+      />
+    </>
+  );
+};
+
+export default MultiRangeSlider;
